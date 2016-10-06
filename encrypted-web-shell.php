d29e7ae36d6e5f0e2c8541ceb3740034f80fccc1
Kevin <shift8web@3057983.no-reply.drupal.org>
2016-10-06 17:57:30 -0400

    adjusted header comment in code

--- encrypted-web-shell.php
+++ encrypted-web-shell.php
@@ -6,22 +6,6 @@ Description :
 This code uses mcrypt to encrpyt raw PHP code (wihtout "<?php ?>").
 It then executes the code after decrypting it. This can potentially aid in the obfuscation (and potentially unrecoverable)
 of your code, whether its shell, penetration testing or remote management and systems administration.
-
-The key item to note here is that the key can be delivered to decrypt + run the code via a http POST, instead of a GET.
-What does this mean? It means that the key you pass to decrypt will be harder to capture and utilize by monitoring parties or 
-via standard apache / nginx access logs. GETs are usually logged, but POSTs are not usually logged without additional 
-custom modules or configuration directives.
-
-Instructions : 
-
-1. Load your code into a text file, read the contents of the file via file_get_contents and use the encrypt function to encrypt it with
-a strong key.
-2. Delete the file and the file_get_contents (save your key)
-3. Paste the encrypted code into the $code variable
-4. Run the file and enter the key in the input box in your browser
-5. It should set a cookie wtih the key and refresh the page with the executed code
-6. Some code or conditions in code may not execute due to it potentially not being escaped properly.
-
 ****************************/
 
 $key = '1234567891011120';
